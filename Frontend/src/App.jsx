0884cebc3cc399a9fb562fd9f76a6ff11c542574
manpreetsingh04m <ms2404mk@gmail.com>
2024-03-01 01:00:31 +0530

    Homepage and navbar added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
-import Login from './components/Login'
+// import Login from './components/Login'
+import Homepage from './components/Homepage'
 
 function App() {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
+      <Homepage/>
     </>
   )
 }
